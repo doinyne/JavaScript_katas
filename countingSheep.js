4ed13c6ca187293de9597c243b298960b24885e3
dean <d.lewis.rave@gmail.com>
2022-02-18 13:23:36 +0000

    updated sheep not submitted

--- countingSheep.js
+++ countingSheep.js
@@ -1,7 +1,10 @@
 var countSheep = function (num){
-  i = num
-
-  if(num === i) {
-    return i + "sheep";
-  };
+  for(let i = 1; i < num; i++) {
+    console.log(i + " sheep");
+  }
 };
+console.log(countSheep(10));
+
+// num = 2 output 1 sheep, 2 sheep
+// num = 5 output 1 sheep, 2 sheep, 3 sheep, 4 sheep, 5 sheep
+// num = 1 output 1 sheep
--- middleChar.js
+++ middleChar.js
@@ -1,26 +1,37 @@
-function getMiddle(s)
+function isEven(s)
 { 
-  if(s % 2 === 0) {
-    return "y";
-  }else if(s % 2 !== 0) {
-    return "x";
+  return s.length % 2 === 0
   };
-}
 
+
+function middle(s) {
+  let mid = s.length / 2;
+  // dean mid = 2
+  // sam mid = 1.5
+  if(isEven(s)) {
+    // if dean return "ea"
+    // if finley return "nl"
+    return s.charAt(mid - 1).concat(s.charAt(mid));
+  }else if(isEven(s) === false) {
+    return s.charAt(mid);
+  };
+};
+console.log(middle("abcde") === "c");
+console.log(middle("abcd") === "bc");
 // odd or even
 
-function oddOrEven(array) {
-   let arrayStr = array.split(",");
-   let sum = arrayStr.reduce(function(total,num){
-     return parseFloat(total) + parseFloat(num);
-   });
-};
+// function oddOrEven(array) {
+//    let arrayStr = array.split(",");
+//    let sum = arrayStr.reduce(function(total,num){
+//      return parseFloat(total) + parseFloat(num);
+//    });
+// };
 
-function sumStr(str){
-  let strArr = str.split(",");
-  let sum = strArr.reduce(function(total, num){
-    return parseFloat(total) + parseFloat(num);
-  });
+// function sumStr(str){
+//   let strArr = str.split(",");
+//   let sum = strArr.reduce(function(total, num){
+//     return parseFloat(total) + parseFloat(num);
+//   });
 
-  return sum;
-}
+//   return sum;
+// }
