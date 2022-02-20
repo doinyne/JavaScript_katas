e652d69a1a667b19768ab352b2501b705a91389a
dean <d.lewis.rave@gmail.com>
2022-02-20 22:17:00 +0000

    sumOfMultiples.js incomplete

---
+++ sumOfMultiples.js
@@ -0,0 +1,14 @@
+function sumMul(n,m){
+  let sum = 0;
+  for(let i = 1; i <= n; i++ == m){
+    console.log(i);
+  };
+};
+console.log(sumMul(10, 10));
+//(0, 0) = invalid
+//(1, 10) = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = 45
+//(3, -3) = invalid
+
+
+// take n and work out the numbers inbetween n & m
+// sum the numbers together
