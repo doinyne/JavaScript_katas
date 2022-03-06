ab6f764b3e445e2bba7e36b611512c2fd7532d86
Dean <dean@Deans-MacBook-Pro.local>
2022-03-06 14:59:30 +0000

    NaN incomplete

---
+++ stringToIntegerToString.js
@@ -0,0 +1,11 @@
+function sumStr(a,b) {
+    let NaN = ""
+    let sum = parseInt(a);
+    let sum2 = parseInt(b);
+    let answer = sum + sum2;
+    
+    
+    return answer.toString();
+  };
+  
+console.log(sumStr("","5"));
