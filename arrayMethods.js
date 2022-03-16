782399fc73315e694e065193058bac96e26134aa
dean <d.lewis.rave@gmail.com>
2022-03-16 10:41:27 +0000

    map and filter methods

--- arrayMethods.js
+++ arrayMethods.js
@@ -3,14 +3,43 @@
 // example array
 
 const items = [
-  { name: 'Bike', price: 100 },
-  { name: 'TV', price: 200 },
-  { name: 'Album', price: 10 },
-  { name: 'Book', price: 5 },
-  { name: 'Phone', price: 500 },
+  { name: 'Bike',     price: 100  },
+  { name: 'TV',       price: 200  },
+  { name: 'Album',    price: 10   },
+  { name: 'Book',     price: 5    },
+  { name: 'Phone',    price: 500  },
   { name: 'Computer', price: 1000 },
-  { name: 'Keyboard', price: 25 }
+  { name: 'Keyboard', price: 25   }
 ]
+
+
+// filer method
+
+const filteredItems = items.filter((item) => {
+  return item.price <= 100
+})
+
+console.log(filteredItems);
+
+// map method
+
+const itemNames = items.map((item) => {
+  return item.name
+})
+
+console.log(itemNames);
+
+const itemPrice = items.map((item) => {
+  return item.price
+})
+
+console.log(itemPrice);
+
+// 
+
+
+
+
 
 
 
