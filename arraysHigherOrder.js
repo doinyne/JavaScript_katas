59e893243502f5ab58d856fe1b73558dcbacb67c
dean <d.lewis.rave@gmail.com>
2022-03-16 15:57:17 +0000

    filter arrays

---
+++ arraysHigherOrder.js
@@ -0,0 +1,18 @@
+var list = [
+  { firstName: 'Noah', lastName: 'M.', country: 'Switzerland', continent: 'Europe', age: 19, language: 'JavaScript' },
+  { firstName: 'Maia', lastName: 'S.', country: 'Tahiti', continent: 'Oceania', age: 28, language: 'JavaScript' },
+  { firstName: 'Shufen', lastName: 'L.', country: 'Taiwan', continent: 'Asia', age: 35, language: 'HTML' },
+  { firstName: 'Sumayah', lastName: 'M.', country: 'Tajikistan', continent: 'Asia', age: 30, language: 'CSS' }
+];
+
+const filteredItems = list.filter((lists) => {
+  return lists.language === 'JavaScript';
+})
+
+const answer = filteredItems.filter((item) => {
+  return item.continent === 'Europe';
+});
+
+const count = answer.length
+
+console.log(count);
