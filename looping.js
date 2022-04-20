d947b3bf56ffa8e55e45e34cf6484496520468ad
Dean Lewis <d.lewis.rave@gmail.com>
2022-04-20 13:23:14 +0100

    parsing array in a function

--- looping.js
+++ looping.js
@@ -1,11 +1,17 @@
-let sandwiches = ['turkey', 'tuna', 'ham', 'pb&j'];
-let numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// let sandwiches = ['turkey', 'tuna', 'ham', 'pb&j'];
+// let numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-function solution(sandwiches) {
-  
+// for ( let i = 0; i < sandwiches.length; i++ ) {
+//   console.log(sandwiches[i]) // returns names
+//   console.log(i) // returns index positions
+// }
+
+// make it working within a function 
+
+function solution(arr) {
+  for ( let i = 0; i < arr.length; i++ ) {
+    console.log(arr[i])
+  }
 }
 
-for ( let i = 0; i < sandwiches.length; i++ ) {
-  console.log(sandwiches[i]) // returns names
-  console.log(i) // returns index positions
-}
+console.log(solution(['turkey', 'tuna', 'ham', 'pb&j']));
