2942fe7f781da38f35693510bdb01a0aedcdec54
Dean Lewis <d.lewis.rave@gmail.com>
2022-04-20 13:15:12 +0100

    return index positions and names from array

--- looping.js
+++ looping.js
@@ -1,1 +1,11 @@
 let sandwiches = ['turkey', 'tuna', 'ham', 'pb&j'];
+let numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+function solution(sandwiches) {
+  
+}
+
+for ( let i = 0; i < sandwiches.length; i++ ) {
+  console.log(sandwiches[i]) // returns names
+  console.log(i) // returns index positions
+}
