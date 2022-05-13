f79dc2f7c4ec6ff2d17238c8326c6eb541f337d8
Dean Lewis <d.lewis.rave@gmail.com>
2022-05-13 17:10:34 +0100

    turney operator fix

--- squashBugsNotDogs.js
+++ squashBugsNotDogs.js
@@ -1,8 +1,11 @@
 function howManyDalmations(number) {
   
   let dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
+
+  let respond = number <= 10 ? dogs[0] : number <= 51 ? dogs[1] : number === 101 ? dogs[3] : number >= 51 ? dogs[2]
+
   
-  let respond = number <= 10 ? dogs[0] : number <= 50 ? dogs[1] : number = 101 ? dogs[3] : number >= 51 ? dogs[2]
+  //  : number <= 50 ? dogs[1] : number = 101 ? dogs[3] : number >= 51 ? dogs[2]
   
 }
 
