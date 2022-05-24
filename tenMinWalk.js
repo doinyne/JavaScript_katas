822c8895cbf34d848fb4c0a757110972dd45a6c7
dean <d.lewis.rave@gmail.com>
2022-05-24 23:03:48 +0100

    wip

--- tenMinWalk.js
+++ tenMinWalk.js
@@ -2,13 +2,12 @@ function isValidWalk(walk) {
   const start = walk[0]
   const length = walk.length
   const end = walk[walk.length - 1]
-  const firstMove = walk[1]
   
-  if(length == 10 && start != firstMove && start === end) {
+  if(length === 10 && start === end) {
     return true;
   } else if (start !== end) {
     return false;
   }
 }
 
-console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s']))
+console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s']))
