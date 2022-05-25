8a29fb7bd57dd61989f87dfb948830d22f5e7ec3
dean <d.lewis.rave@gmail.com>
2022-05-25 11:39:57 +0100

    unfinished - removed hash

--- bingoOrNot.js
+++ bingoOrNot.js
@@ -1,40 +1,10 @@
 function bingo(a) {
-  let arr = new Map([ 
-    [ 1, "a" ],
-    [ 2, "b" ],
-    [ 3, "c" ],
-    [ 4, "d" ],
-    [ 5, "e" ],
-    [ 6, "f" ],
-    [ 7, "g" ],
-    [ 8, "h" ],
-    [ 9, "i" ],
-    [ 10, "j" ],
-    [ 11, "k" ],
-    [ 12, "l" ],
-    [ 13, "m" ],
-    [ 14, "n" ],
-    [ 15, "o" ],
-    [ 16, "p" ],
-    [ 17, "q" ],
-    [ 18, "r" ],
-    [ 19, "s" ],
-    [ 20, "t" ],
-    [ 21, "u" ],
-    [ 22, "v" ],
-    [ 23, "w" ],
-    [ 24, "x" ],
-    [ 25, "y" ],
-    [ 26, "z" ],
-  ]);
-
   result = []
 
-  arr.forEach((letter, index) => {
+  a.forEach((letter, index) => {
     if( a.includes(letter)) {
       result.push(index)
     }
-
   })
   return result
 };
