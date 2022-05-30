f707d8793b1879813bcca1abfc254ce29d4d3df3
dean <d.lewis.rave@gmail.com>
2022-05-30 20:49:21 +0100

    concatination with variable to make a sentence

--- freecodecampnotes.js
+++ freecodecampnotes.js
@@ -1,21 +1,36 @@
 // In order to get the last letter of a string, you can subtract one from the string's length.
 
 //For example, if const firstName = "Ada", you can get the value of the last letter of the string by using firstName[firstName.length - 1].
-
+/*
 const firstName = "Ada";
 const lastLetter = firstName[firstName.length - 1];
 
 // lastLetter would have a value of the string a.
 
 console.log(lastLetter);
+*/
 
 // Use Bracket Notation to Find the Nth-to-Last Character in a String
 
 // You can use the same principle we just used to retrieve the last character in a string to retrieve the Nth-to-last character.
 
 //For example, you can get the value of the third-to-last letter of the const firstName = "Augusta" string by using firstName[firstName.length - 3]
-
+/*
 const firstNameTwo = "Augusta";
 const thirdToLastLetter = firstNameTwo[firstNameTwo.length - 3];
 
 console.log(thirdToLastLetter);
+
+*/
+/*
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "The " + myAdjective + " hairy " + myNoun + " Steve, " + myVerb + " to me " + myAdverb + ".";
+
+console.log(wordBlanks);
+*/
+//
