8e441da4bea1bd6b3837a30bb60bbda47e346864
dean <d.lewis.rave@gmail.com>
2022-06-03 16:37:02 +0100

    wip

---
+++ hallo.js
@@ -0,0 +1,13 @@
+function validateHello(greetings) {
+  if (greetings === "hello" ? true
+       : "ciao" ? true
+       : "salut" ? true
+       : "hallo" ? true
+       : "hola" ? true
+       : "ahoj" ? true
+       : "czesc" ? true;
+       )
+
+}
+
+console.log(validateHello("no"))
