891f5bfb8f55a10a2f33831b098dad46cebd7a2f
dean <d.lewis.rave@gmail.com>
2022-06-03 14:53:28 +0100

    how many days in a month switch

--- conditionalStatementSwitch.js
+++ conditionalStatementSwitch.js
@@ -1,7 +1,44 @@
 function howManydays(month){
   var days;
-  switch (){
-
+  switch (month){
+    case 1:
+      return 31
+      break;
+    case 2:
+      return 28
+      break;
+    case 3:
+      return 31
+      break;
+    case 4:
+      return 30
+      break;
+    case 5:
+      return 31
+      break;
+    case 6:
+      return 30
+      break;
+    case 7:
+      return 31
+      break;
+    case 8:
+      return 31
+      break;
+    case 9:
+      return 30
+      break;
+    case 10:
+      return 31
+      break;
+    case 11:
+      return 30
+      break;
+    case 12:
+      return 31
+      break;
   }
   return days;
 }
+
+console.log(howManydays(5))
