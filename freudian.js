b75baaf483536ddb648fa6dac202996eb1021f93
dean <d.lewis.rave@gmail.com>
2022-06-06 23:18:42 +0100

    cant work out how to return nothing

---
+++ freudian.js
@@ -0,0 +1,12 @@
+function toFreud(string) {
+  let arr = string.split(' ');
+
+  let arr2 = []
+
+  for (var i = 1; i <= arr.length; i++) arr2.push("sex");
+
+  return arr2;  
+
+}
+
+console.log(toFreud(""));
