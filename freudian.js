013fc39517f832c325dddf5f9eeaec5017197c4a
dean <d.lewis.rave@gmail.com>
2022-06-07 00:12:22 +0100

    wip

--- freudian.js
+++ freudian.js
@@ -1,12 +1,11 @@
 function toFreud(string) {
   let arr = string.split(' ');
-
   let arr2 = []
 
-  for (var i = 1; i <= arr.length; i++) arr2.push("sex");
-
-  return arr2;  
-
+  for (var i = 1; i <= arr.length; i++){
+    arr2.push("sex");
+  }
+  return arr2.join(" "); 
 }
 
-console.log(toFreud(""));
+console.log(toFreud("Dean Lewis"));
