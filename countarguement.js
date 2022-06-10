d2bd243c3149ead1b51f202cd89ef687989f835d
dean <d.lewis.rave@gmail.com>
2022-06-10 22:06:20 +0100

    countargument

---
+++ countarguement.js
@@ -0,0 +1,11 @@
+function strCount(str, letter){  
+  let count = 0;
+  for ( i = 0; i < str.length; i++ ) {
+    if(i === letter) {
+      count.push[str[i]] + i++;
+    } else return 0;
+  } 
+  return count;
+}
+
+console.log(strCount(('Hello', 'o'), 1)))
