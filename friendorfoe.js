3bf3771e97922738975a21e76fe3ee584097432d
dean <d.lewis.rave@gmail.com>
2022-06-10 14:50:54 +0100

    friend or foe dean version

---
+++ friendorfoe.js
@@ -0,0 +1,19 @@
+
+// my first attemp
+
+function friend(friends){
+  let friendsList = [];
+  for(let i = 0; i < friends.length; i++) {
+    if( friends.length = 3) {
+      friendsList.push(friends[i])
+    } 
+    return friendsList
+  }
+}
+
+console.log(friend(["Ryan", "Mark", "Kieran"]))
+
+// push names of people to new array called friends if their name is 4 characters long
+
+
+// youtube walk through 
