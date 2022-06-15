da8025237d6158efbf037e33338909adf831c115
dean <d.lewis.rave@gmail.com>
2022-06-15 12:35:46 +0100

    for loop for every letter

--- uppercaseeveryother.js
+++ uppercaseeveryother.js
@@ -1,5 +1,9 @@
 function spongeMeme(sentence) {
+  let str = sentence.toLowerCase();
   
+  for(let i = 0; i < str.length; i++) {
+
+  }
 }
 
 console.log(spongeMeme("stop Making spongebob Memes!"))
