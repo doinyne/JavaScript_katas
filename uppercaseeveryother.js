2e8b91f07435154258c77215abf4bd359910dcc9
dean <d.lewis.rave@gmail.com>
2022-06-15 12:55:47 +0100

    wip

--- uppercaseeveryother.js
+++ uppercaseeveryother.js
@@ -1,8 +1,11 @@
 function spongeMeme(sentence) {
-  let str = sentence.toLowerCase();
+  let str = sentence.toLowerCase().split('');
   
-  for(let i = 0; i < str.length; i++) {
-
+  
+  for (let i = 0; i < str.length; i++) {
+    if( str[i] === str[i].toLowerCase ()) {
+      console.log(str[i].toUpperCase() + str[i]); 
+    }
   }
 }
 
