b36679c5f468f92f7bde0154e8bcc2f46a7308d6
dean <d.lewis.rave@gmail.com>
2022-06-16 14:07:06 +0100

    wip

---
+++ lengthandvalue.js
@@ -0,0 +1,6 @@
+function alternate(n, firstValue, secondValue){
+  let arr = [firstValue, secondValue];
+  let foo = arr.length * n
+  console.log(arr(foo)) 
+}
+console.log(alternate(5, "true", "false"))
