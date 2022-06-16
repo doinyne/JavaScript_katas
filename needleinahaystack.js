4486744675deccacbcbf226ead26f7ccd44f684c
dean <d.lewis.rave@gmail.com>
2022-06-16 14:44:31 +0100

    added in a refactored version

--- needleinahaystack.js
+++ needleinahaystack.js
@@ -10,4 +10,10 @@ function findNeedle(haystack) {
 
 }
 
+/*
+function findNeedle(haystack) {
+  return "found the needle at position " + haystack.indexOf("needle");
+}
+*/
+
 console.log(findNeedle(['3', '123124234', undefined, 'world', 'needle', 'hay', 2, '3', true, false]))
