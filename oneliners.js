a4667e10a0015d1489a0672a75c55f90e5b5a70a
dean <d.lewis.rave@gmail.com>
2022-06-20 17:44:29 +0100

    remove all occurences

--- oneliners.js
+++ oneliners.js
@@ -3,4 +3,8 @@ var array = ["A", "B", "C", "D", "B", "B"]
 // REMOVE ONLY THE FIRST
 console.log(array.splice(array.indexOf('B'), 1));
 
+// REMOVE ONLY THE LAST 
 console.log(array.splice(array.lastIndexOf('B'), 1));
+
+// REMOVE ALL OCCURENCES
+console.log(array = array.filter(v => v !== 'B'));
