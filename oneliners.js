4a95ff14204dad2e2d0ba01bcc7b528332c3d258
dean <d.lewis.rave@gmail.com>
2022-06-20 17:42:52 +0100

    remove only the last

--- oneliners.js
+++ oneliners.js
@@ -1,5 +1,6 @@
-var array = ["A", "B", "C", "D", "B"]
+var array = ["A", "B", "C", "D", "B", "B"]
 
 // REMOVE ONLY THE FIRST
 console.log(array.splice(array.indexOf('B'), 1));
 
+console.log(array.splice(array.lastIndexOf('B'), 1));
