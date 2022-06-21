63d3f12cc6258370bc4d599a31388444b6f867f9
dean <d.lewis.rave@gmail.com>
2022-06-21 13:03:21 +0100

    save for lunch

--- hackerrank/day1/question3.js
+++ hackerrank/day1/question3.js
@@ -8,11 +8,17 @@ function timeConversion(s) {
   console.log(arr2[0] + arr2[1])
 
   if(time.includes('P', 0)) {
+
     return Number(arr3[0]) + Number(12) + ":" + arr3[1] + ":" + arr3[2].split(/(PM|AM)/gm)[0]
+
   } else if (arr2[0] + arr2[1].includes('12') && arr2[8].includes('A')) {
+
     return Number(arr3[0]) - Number(12) + '0' + ":" + arr3[1] + ":" + arr3[2].split(/(PM|AM)/gm)[0]
+
   } else {
+
     return s.split(/(PM|AM)/gm)[0]
+    
   }
 }
 
