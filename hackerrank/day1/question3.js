e42b3ac548d22f7e707f4b4077df9c820cb5124c
dean <d.lewis.rave@gmail.com>
2022-06-21 12:36:18 +0100

    12 am turns to 00:00 but for 1 am it turns to -110. i need to isolate the time with am too

--- hackerrank/day1/question3.js
+++ hackerrank/day1/question3.js
@@ -1,4 +1,15 @@
 function timeConversion(s) {
-  
+  let time = s.split('');
+  let arr2 = time.join('')
+  let arr3 = arr2.split(':')
 
+  if(time.includes('P', 0)) {
+    return Number(arr3[0]) + Number(12) + ":" + arr3[1] + ":" + arr3[2].split(/(PM|AM)/gm)[0]
+  } else if (arr2.includes(12 && 'AM')) {
+    return Number(arr3[0]) - Number(12) + '0' + ":" + arr3[1] + ":" + arr3[2].split(/(PM|AM)/gm)[0]
+  } else {
+    return s.split(/(PM|AM)/gm)[0]
+  }
 }
+
+console.log(timeConversion("12:05:45AM"))
