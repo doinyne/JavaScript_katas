b42f9b9269ed9efbe0606b7b8fc36713b5160a03
dean <d.lewis.rave@gmail.com>
2022-06-21 18:26:31 +0100

    day 1 complete

--- hackerrank/day1/question3.js
+++ hackerrank/day1/question3.js
@@ -5,21 +5,23 @@ function timeConversion(s) {
   console.log(arr2)
   let arr3 = arr2.split(':')
   console.log(arr3)
-  console.log(arr2[0] + arr2[1])
+  let indexFirst = arr2[0] + arr2[1] + arr2[8] + arr2[9]
+  console.log(indexFirst)
 
-  if(time.includes('P', 0)) {
+  if(time.includes('P', 0) && Number(arr3[0] + arr3[1] <= "12")) {
 
     return Number(arr3[0]) + Number(12) + ":" + arr3[1] + ":" + arr3[2].split(/(PM|AM)/gm)[0]
 
-  } else if (arr2[0] + arr2[1].includes('12') && arr2[8].includes('A')) {
+  } else if (indexFirst === "12AM") {
 
     return Number(arr3[0]) - Number(12) + '0' + ":" + arr3[1] + ":" + arr3[2].split(/(PM|AM)/gm)[0]
 
   } else {
 
     return s.split(/(PM|AM)/gm)[0]
-    
+
   }
 }
 
 console.log(timeConversion("01:05:45AM"))
+
