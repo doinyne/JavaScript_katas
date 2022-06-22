80ef2247ad7fef9a0dc99a81c2424fa07c5de430
dean <d.lewis.rave@gmail.com>
2022-06-22 13:01:43 +0100

    question1 done

--- hackerrank/day2/question1.js
+++ hackerrank/day2/question1.js
@@ -1,15 +1,13 @@
 function lonelyinteger(a) {
   let arr = a.sort();
   let answer = [];
-  let storage = [];
   
   for (let i = 0; i < arr.length; i++) {
-    if(arr[i] === arr[i + 1]) {
-      storage.push(arr[i])
-    } else if(arr[i] != arr[i + 1]){
-      answer.push(arr[i])
+    if(arr[i] != arr[i + 1]) {
+      answer.push(arr[i]);
+    } else {
+      i++;
     }
-    console.log(arr[i])
   }
   return answer
 }
