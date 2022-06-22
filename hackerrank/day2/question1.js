a7a9d9fd8037920fd7a073855aeff128663c4ea7
dean <d.lewis.rave@gmail.com>
2022-06-22 12:46:31 +0100

    adding single accurances to new array

--- hackerrank/day2/question1.js
+++ hackerrank/day2/question1.js
@@ -1,4 +1,17 @@
 function lonelyinteger(a) {
-  // Write your code here
-
+  let arr = a.sort();
+  let answer = [];
+  let storage = [];
+  
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] === arr[i + 1]) {
+      storage.push(arr[i])
+    } else if(arr[i] != arr[i + 1]){
+      answer.push(arr[i])
+    }
+    console.log(arr[i])
+  }
+  return answer
 }
+
+console.log(lonelyinteger([1,2,3,4,3,2,1]))
