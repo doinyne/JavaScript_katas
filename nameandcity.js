cf28960f25a230a37158138c4a6c70648bc5ccda
dean <d.lewis.rave@gmail.com>
2022-06-24 00:26:35 +0100

    save

--- nameandcity.js
+++ nameandcity.js
@@ -1,3 +1,9 @@
 function sayHello( name, city, state ) {
-  
+  let names = name.join(',').replace(",", " ");
+  console.log(names)
+  return "Hello, " + names + "!" + " Welcome to " + city + ", " + state + "!";
 }
+
+console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona'))
+
+// 'Hello, John Smith! Welcome to Phoenix, Arizona!'
