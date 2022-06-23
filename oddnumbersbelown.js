4897b4ea0e71afa0b28a49b29c8ba88e1c982e60
dean <d.lewis.rave@gmail.com>
2022-06-23 23:23:50 +0100

    my solution

--- oddnumbersbelown.js
+++ oddnumbersbelown.js
@@ -1,12 +1,13 @@
 function oddCount(n){
   let odd = []
-  for(var i = 1; i <= n; i++) {
+  
+  for(var i = 1; i <=n; i++) {
     if(i % 2 != 0) {
       odd.push(i)
     }
    }
-   console.log(odd)
-  return odd.length -1
+   return odd.length
 }
 
-console.log(oddCount(15023));
+console.log(oddCount(40));
+
