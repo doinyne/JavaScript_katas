437b97f84f40333cb031cf1698600728674c3e79
dean <d.lewis.rave@gmail.com>
2022-06-23 22:54:01 +0100

    failing some tests

--- oddnumbersbelown.js
+++ oddnumbersbelown.js
@@ -1,5 +1,12 @@
 function oddCount(n){
-  
+  let odd = []
+  for(var i = 1; i <= n; i++) {
+    if(i % 2 != 0) {
+      odd.push(i)
+    }
+   }
+   console.log(odd)
+  return odd.length -1
 }
 
-console.log(oddCount(20));
+console.log(oddCount(15023));
