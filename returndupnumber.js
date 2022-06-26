6f8ebe8f5ef2751700dca5dc1560c5cceb8a794c
Dean Lewis <d.lewis.rave@gmail.com>
2022-06-26 19:15:39 +0100

    stuck

--- returndupnumber.js
+++ returndupnumber.js
@@ -1,5 +1,14 @@
 function findDup(arr) {
-
+  let dup = []
+  let foo = arr.sort()
+  for(let i = 0; i <= foo.length; i++) {
+    if(i === foo[i]) {
+      dup.push(i)
+    } else {
+      i++;
+    }
+  }
+  return dup
 }
 
-console.log(findDup([1,2,2,3]))
+console.log(findDup([1,3,2,5,4,5,7,6]))
