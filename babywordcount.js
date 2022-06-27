eabf3c1a7411128f3267cce91331d4d8a2f8b261
dean <d.lewis.rave@gmail.com>
2022-06-27 22:15:02 +0100

    stuck

--- babywordcount.js
+++ babywordcount.js
@@ -1,3 +1,28 @@
 function babyCount(x) {
-  // good luck - you'll need it!
+  let count = [];
+  let baby = "baby"
+  
+
+  for(let i = 0; i < x.length; i++) {
+    if(x[i] === "b") {
+      count.push(x[i])
+    } else if(x[i] === "a"){
+      count.push(x[i])
+    } else if(x[i] === "y"){
+      count.push(x[i])
+    } else {
+      i++;
+    }
+  }
+  console.log(count.join(""))
+  let total = 0;
+  for(let i = 0; i < count.length; i++) {
+    if(count.includes(baby) === true) {
+      total += 1;
+    }
+  }
+  return total;
 }
+
+console.log(babyCount('babyhellohellobabybaby'))
+
