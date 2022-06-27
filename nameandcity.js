e47362922c3209be8a0d74bd7543a2df56ddcc5a
dean <d.lewis.rave@gmail.com>
2022-06-27 18:10:17 +0100

    passing before refactoring

--- nameandcity.js
+++ nameandcity.js
@@ -1,9 +1,14 @@
 function sayHello( name, city, state ) {
-  let names = name.join(',').replace(",", " ");
-  console.log(names)
+  let names = name.join(',').replace(",", ' ').replace(",", " ").replace(",", " ");
+  console.log( names );
+  console.log(name.length)
+  if (name.length > 2) {
   return "Hello, " + names + "!" + " Welcome to " + city + ", " + state + "!";
+  } else {
+    return "Hello, " + names + "!" + " Welcome to " + city + ", " + state + "!";
+  }
 }
 
-console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona'))
+console.log(sayHello(['Franklin','Delano','Roosevelt'], 'Chicago', 'Illinois'))
 
 // 'Hello, John Smith! Welcome to Phoenix, Arizona!'
