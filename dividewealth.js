622f9123475adc8e3601e86703cada3ec6bad434
dean <d.lewis.rave@gmail.com>
2022-06-27 23:21:45 +0100

    best practise

--- dividewealth.js
+++ dividewealth.js
@@ -1,3 +1,17 @@
 function redistributeWealth(wealth) {
+  var sum = 0;
+  var average = 0;
+
+  for(var i = 0;i<wealth.length;i++)
+  {
+    sum += wealth[i];
+  }
+  average = sum / wealth.length;
   
+  for(var i = 0;i<wealth.length;i++)
+  {
+    wealth[i] = average;
+  }
 }
+
+console.log(redistributeWealth([5, 10, 6]))
