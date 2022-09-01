31a3978856677d02412ace764e2eaf708ebc3d18
dean <d.lewis.rave@gmail.com>
2022-09-01 16:37:44 +0100

    managed to fill the array correctly, just not passing tests on codewars

--- allNumbers.js
+++ allNumbers.js
@@ -1,3 +1,15 @@
+let arrayNumbers = []
+
 function arr(n) {
-  
+
+  for(var i = 0; i < n; i++) {
+    if(n > i) {
+      arrayNumbers.push(i)
+    } else {
+      return arrayNumbers
+    }
+  }
 }
+
+console.log(arrayNumbers)
+console.log(arr())
