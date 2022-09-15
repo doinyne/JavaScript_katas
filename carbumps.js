51acbc6512b5c9e70f2d6642f656c9bc0166944b
dean <d.lewis.rave@gmail.com>
2022-09-15 14:54:31 +0100

    wip car bumps

--- carbumps.js
+++ carbumps.js
@@ -1,21 +1,15 @@
 function bump(x){
-  // let roadBumps = 0;
-  // let array = x.split("");
-  // console.log(array.length)
-  // for(let i = 0; i < array.length; i++) {
-  //   if(array[i] === "n"){
-  //     roadBumps += 1
-  //   } else if(array[i] === "_") {
-  //     i++;
-  //   }
-  // }
-  // return roadBumps
-  
-  // if(roadBumps < 15) {
-  //   return "Woohoo!"
-  // } else {
-  //   return "Car Dead"
-  // }
+  let roadBumps = 0;
+  let array = x.split("");
+  console.log(array.length)
+  for(let i = 0; i < array.length; i++) {
+    if(i === "n"){
+      roadBumps += 1
+    } else if(i === "_") {
+      i++;
+    }
+    return roadBumps
+  }
 }
 
 console.log(bump("_n__nnn_nnnnnn___nn___n_nn_nn"))
