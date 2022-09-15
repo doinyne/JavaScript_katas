1d8d598607d1b7d2f84b30119ffb24d29c5a70f4
dean <d.lewis.rave@gmail.com>
2022-09-15 15:55:43 +0100

    my whiteboarding question

---
+++ anograms.js
@@ -0,0 +1,13 @@
+function anograms(wordOne, wordTwo) {
+  let word1 = wordOne.split('').sort().join('');
+  let word2 = wordTwo.split('').sort().join('');
+  
+
+  if(word1 === word2) {
+    return true;
+  } else {
+    return false;
+  }
+  
+}
+console.log(anograms('abab', 'abba'));
